e992299049debef6d0feff691b7883f51c0053a7
FinTheHuman <edward@Edwards-Air.home>
2015-11-25 07:40:09 -0500

    Fixed the bug

--- wordnik.js
+++ wordnik.js
@@ -1,4 +1,5 @@
-function getRandomWord(callback){
+$(document).ready(function(){
+  function getRandomWord(callback){
   $.ajax({
   url: "http://api.wordnik.com/v4/words.json/randomWords?hasDictionaryDef=true&minCorpusCount=0&minLength=5&maxLength=15&limit=1&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5"
 }).done(function(data) {
@@ -52,3 +53,4 @@ console.log(data);
     
 });
 }
+});
